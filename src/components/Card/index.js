4e931fa814d060bb1e89f91e5d92ccc8f364495d
Joas <joassantos30@hotmail.com>
2021-02-20 13:49:58 -0300

    Criando componente card

---
+++ src/components/Card/index.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import "./styles.css";
+
+class Card extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className="data-inscricao">
+                    <h2>EXPOSIÇÃO DE FOTOS</h2>
+                    <p>20 á 24/03/2017 na divisórias da Ouvidoria
+                                        (em frente ao elevador)</p>
+                    <h2>APRESENTAÇÃO</h2>
+                    <p>20/03/2017 das 16:30h ás 17:30h.</p>
+                </div>
+                <div className="botao-incricao">
+                    <button id="myBtn-2" style={{ cursor: "pointer" }}>FAÇA SUA INSCRIÇÃO</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Card;
