3ae8296776606db3ad4bf824db1a491361675bcf
Joas <joassantos30@hotmail.com>
2021-02-21 15:20:01 -0300

    Estilizando a label dos componentes de input

--- src/components/InputFile/index.js
+++ src/components/InputFile/index.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
+import "./styles.css";
 
 class InputFile extends Component {
   render() {
     return (
-      <div>
+      <div className="input-file">
         <label for={this.props.name}>{this.props.label}</label>
         <input type="file" id={this.props.name} name={this.props.name} />
         <br />
