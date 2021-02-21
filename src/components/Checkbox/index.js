57a97d3ef9516d2e06c16f4e223a669c243e794d
Joas <joassantos30@hotmail.com>
2021-02-21 15:17:54 -0300

    Estilizando a label do componente checkbox

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -4,7 +4,7 @@ import "./styles.css";
 class Checkbox extends Component {
   render() {
     return (
-      <div className="c-termos">
+      <div className="check">
         <input type="checkbox" id={this.props.name} name={this.props.name} />
         <label for={this.props.name}>{this.props.label}</label>
       </div>
